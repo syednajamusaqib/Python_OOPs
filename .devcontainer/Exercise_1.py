68b1573f301949f33ab05734e09e1a12c55753ca
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2024-02-13 17:07:23 +0500

    Deleted

--- .devcontainer/Exercise_1.py
+++
@@ -1,8 +0,0 @@
-# OOP Exercise 1: Create a Class with instances and attributes
-class Vehicle(object):
-    def __init__(self,max_speed,mileage):
-        self.max_speed = max_speed
-        self.mileage = mileage
-# Creating Object for Vehicle Class    
-car = Vehicle(67,13000)
-print(f"Mileage of your car is {car.mileage}")
--- .devcontainer/Exercise_2.py
+++
@@ -1,3 +0,0 @@
-# OOP Exercise 2: Create a Vehicle class with no attributes and methods
-class vehicle(object):
-    pass
